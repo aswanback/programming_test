16a79ac6e45b261e6042befc880d1e36c80209fc
andrewswanback <atswanback@gmail.com>
2021-03-12 18:04:17 -0500

    made fibonnaci on one line

--- fibonnacci.java
+++ fibonnacci.java
@@ -3,17 +3,17 @@ class Scratch {
         int two_previous = 0;
         int previous = 1;
         int result = 0;
-        System.out.println(two_previous);
-        System.out.println(previous);
+        System.out.format("%d",two_previous);
+        System.out.format(", %d",previous);
         while(result != n) {
             result = previous + two_previous;
-            System.out.println(result);
+            System.out.format(", %d",result);
             two_previous = previous;
             previous = result;
         }
     }
 
     public static void main(String[] args) {
-        Fibonacci(34);
+        Fibonacci(34);  //prints each fibbonacci number on a newlein
     }
 }
